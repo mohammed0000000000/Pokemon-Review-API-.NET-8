b806e741bbd698af4c3650817fea6522a3aa9aa2
Muhammed Maklad <mohammedmaklad97@gmail.com>
2024-10-13 23:43:51 +0300

    many to one relationships

--- Models/Review.cs
+++ Models/Review.cs
@@ -6,5 +6,11 @@
 		public string Title { get; set; }
         public string Text { get; set; }
 
+		public int PokemonId { get; set; }	
+		public Pokemon Pokemon { get; set; }
+
+		public int ReviewerId {  get; set; }	
+		public Reviewer Reviewer { get; set; }	
+
     }
 }
