0e89f1851f810a594c277f6df400e2190d19f70c
Muhammed Maklad <mohammedmaklad97@gmail.com>
2024-10-13 22:50:32 +0300

    models v2

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -2,5 +2,9 @@
 {
 	public class Pokemon
 	{
-	}
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; } 
+
+    }
 }
--- Models/Review.cs
+++ Models/Review.cs
@@ -2,5 +2,9 @@
 {
 	public class Review
 	{
-	}
+		public int Id { get; set; }
+		public string Title { get; set; }
+        public string Text { get; set; }
+
+    }
 }
